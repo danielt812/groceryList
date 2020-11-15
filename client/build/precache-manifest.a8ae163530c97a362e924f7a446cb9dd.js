f7ebf0dea2b96cbc3d2ea1eaa889a3e530c7c8a4
Daniel Tolan <danieltolan@gmail.com>
2020-11-15 13:05:09 -0600

    creating build

---
+++ client/build/precache-manifest.a8ae163530c97a362e924f7a446cb9dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7063abc966ccf752aab6932bb78c74b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d91cdf42653932d73346",
+    "url": "/static/css/main.7bc0d038.chunk.css"
+  },
+  {
+    "revision": "a2e1489eeb80662d43e1",
+    "url": "/static/js/2.3c332084.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.3c332084.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d91cdf42653932d73346",
+    "url": "/static/js/main.4d4ba4cc.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
