4a8889eeca2c234ceeb0446df2fd5441059554fc
Daniel Tolan <danieltolan@gmail.com>
2020-11-16 16:32:25 -0600

    debug dotenv files

--- server.js
+++ server.js
@@ -36,6 +36,8 @@ if (process.env.NODE_ENV === 'production') {
 	});
 }
 
+console.log(process.env.mongoURI);
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
